92a75e9d92f7d31b095f395fc5ca16cd5b2b6445
sharon884 <sharonsharon9048461583@gmail.com>
2026-05-30 15:24:08 +0530

    Setup Express server and MongoDB connection

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+    try {
+        await connectDB();
+
+        app.listen(PORT, () => {
+            console.log(`Server Running On Port ${PORT}`);
+        });
+
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+startServer();
